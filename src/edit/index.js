a7931d72e2dec213bd18e07e4cdcebb81b57bd0d
roylin <1002591652@qq.com>
2020-12-03 18:40:01 +0800

    feat: fork

--- src/edit/index.js
+++ src/edit/index.js
@@ -1,52 +1,47 @@
 /**
  * @description 编辑器打包总入口
  */
-import { Headers, DOMIN, hookMap } from '../global';
-import React from 'react';
-import ReactDom from 'react-dom';
-import Board from './board';
-import { searchTree, EnumEdit } from './common';
-import App from '../reducer';
-import { message } from 'antd';
-import { setTwoToneColor } from '@ant-design/icons';
-import './style/antd.less';
+import { Headers, DOMIN, hookMap } from "../global";
+import React from "react";
+import ReactDom from "react-dom";
+import Board from "./board";
+import { searchTree, EnumEdit } from "./common";
+import App from "../reducer";
+import { message } from "antd";
+import { setTwoToneColor } from "@ant-design/icons";
+import "./style/antd.less";
 
-window.ENV = 'edit';
+window.ENV = "edit";
 
-setTwoToneColor('#ec78cf');
+setTwoToneColor("#ff0000");
 
 // 拉取当前页面的JSON配置、组件菜单后再渲染编辑器
 Promise.all([
-    fetch(DOMIN + '/loadPage', {
-        method: 'POST',
-        headers: Headers.json
-    }).then(response => response.json()),
-    fetch(DOMIN + '/getCompMenu', {
-        method: 'POST',
-        headers: Headers.json
-    }).then(response => response.json())
+  fetch(DOMIN + "/loadPage", {
+    method: "POST",
+    headers: Headers.json,
+  }).then((response) => response.json()),
+  fetch(DOMIN + "/getCompMenu", {
+    method: "POST",
+    headers: Headers.json,
+  }).then((response) => response.json()),
 ]).then(([resPage, resMenu]) => {
-    if (resPage.error !== 0) {
-        message.error(resPage.msg);
-        return;
-    }
-    if (resMenu.error !== 0) {
-        message.error(resMenu.msg);
-        return;
-    }
-    const { tree, hook, page, pid } = resPage.data;
+  if (resPage.error !== 0) {
+    message.error(resPage.msg);
+    return;
+  }
+  if (resMenu.error !== 0) {
+    message.error(resMenu.msg);
+    return;
+  }
+  const { tree, hook, page, pid } = resPage.data;
 
-    Object.assign(hookMap, hook);
-    searchTree(tree, null, EnumEdit.maxKeyNum);
-    ReactDom.render(
-        <App
-            tree={tree}
-            page={page}
-            pid={pid}
-            menu={resMenu.data}
-        >
-            <Board />
-        </App>
-        , document.getElementById('edit')
-    );
+  Object.assign(hookMap, hook);
+  searchTree(tree, null, EnumEdit.maxKeyNum);
+  ReactDom.render(
+    <App tree={tree} page={page} pid={pid} menu={resMenu.data}>
+      <Board />
+    </App>,
+    document.getElementById("edit")
+  );
 });
